4d3b9a2c15994da014c14c12dd7b09949fe7cbb5
William Huang <wilhu@upenn.edu>
2025-01-05 20:28:02 -0500

    initial project setup + form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState } from 'react';
+import './Form.css';
+
+const Form = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Name: ${name} Email: ${email} Message: ${message}`);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Enter your name: </label>
+            <input type="text" onChange={(e) => setName(e.target.value)}/>
+            <br></br>
+            <label>Enter your email: </label>
+            <input type="text" onChange={(e) => setEmail(e.target.value)}/>
+            <br></br>
+            <label>Enter your message: </label>
+            <input type="text" onChange={(e) => setMessage(e.target.value)}/>
+            <br></br>
+            <input type="submit" />
+        </form>
+    );
+};
+
+export default Form;
