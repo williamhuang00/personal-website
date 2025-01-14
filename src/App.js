ca607c51eeffb1b342d478af612928db6a98d8e9
William Huang <wilhu@upenn.edu>
2025-01-13 21:06:56 -0500

    I created a search functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
-import Form from './components/Form/Form';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home';
@@ -10,8 +10,36 @@ import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 
+const users = [
+  { firstName: "John", id: 1 },
+  { firstName: "Emily", id: 2 },
+  { firstName: "Michael", id: 3 },
+  { firstName: "Sarah", id: 4 },
+  { firstName: "David", id: 5 },
+  { firstName: "Jessica", id: 6 },
+  { firstName: "Daniel", id: 7 },
+  { firstName: "Olivia", id: 8 },
+  { firstName: "Matthew", id: 9 },
+  { firstName: "Sophia", id: 10 }
+]
 
 function App() {
+  const [searchItem, setSearchItem] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value; //searchTerm, leveraged to filter on the users array to return filtered copy
+
+    setSearchItem(searchTerm);
+    
+    const filteredItems = users.filter(user => {
+      //filter conditions go here
+      return user.firstName.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    
+    setFilteredUsers(filteredItems);
+  }
+
   return (
     <Router>
       <Header />
@@ -21,6 +49,15 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
       </Routes>
+      <input
+        type="text"
+        value={searchItem}
+        onChange={handleInputChange}
+        placeholder='Type to search'
+      ></input>
+      <ul>
+        {filteredUsers.map(user => <li key={user.id}>{user.firstName}</li>) /* presentation of each object */} 
+      </ul>
       <Footer></Footer>
     </Router>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Hi, this is William</h1>
+
+        <img></img>
+    </div>
   )
 }
 
