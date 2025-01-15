5975ce8d003532043411001ce43a658569943607
William Huang <wilhu@upenn.edu>
2025-01-15 18:35:19 -0500

    playing around with data tables in react

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+import DataTable from 'datatables.net-react'
+import DT from 'datatables.net';
+  
+DataTable.use(DT);
 
+//one of these properties can be a url link, 
+//or have a value that can be leveraged to direct the user to a specific route
 const users = [
   { firstName: "John", id: 1, metadata: 'ab9283jrjasodfj2m' },
   { firstName: "Emily", id: 2, metadata: 'ab9283jrjasodfj2m'  },
@@ -20,8 +26,13 @@ const users = [
   { firstName: "Daniel", id: 7, metadata: '3923094kjasdf'  },
   { firstName: "Olivia", id: 8, metadata: 'ab9283jrjasodfj2m'  },
   { firstName: "Matthew", id: 9, metadata: 'ab9283jrjasodfj2m' },
-  { firstName: "Sophia", id: 10, metadata: 'ab9283jrjasodfj2m'  }
+  { firstName: "Sophia", id: 10, metadata: 'ab9283jrjasodfj2m'  },
+  { firstName: "William", id: 11, metadata: 'ab9283jrjasodfj2m'  },
+  { firstName: "Isabella", id: 12, metadata: 'ab9283jrjasodfj2m'  },
+  { firstName: "Ethan", id: 13, metadata: 'ab9283jrjasodfj2m'  },
 ]
+
+
 
 function App() {
   const [searchItem, setSearchItem] = useState('');
@@ -58,6 +69,23 @@ function App() {
       <ul>
         {filteredUsers.map(user => <li key={user.id}>{user.firstName + " " + user.metadata}</li>) /* presentation of each object */} 
       </ul>
+      <DataTable>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Metadata</th>
+            </tr>
+            
+        </thead>
+        <tbody>
+          {filteredUsers.map(user => (
+            <tr key={user.id}>
+                <td>{user.firstName}</td>
+                <td>{user.metadata}</td>
+            </tr>
+          ))}
+        </tbody>
+      </DataTable>
       <Footer></Footer>
     </Router>
   );
